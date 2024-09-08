0c01e510765a71102c73fa9944603699f05521b7
Innocent Anozie <iygeal@gmail.com>
2024-09-08 13:33:21 +0100

    TASK 7: Test the has_license method with parameterized multiple cases.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -90,6 +90,20 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/google/repos")
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+        ({}, "my_license", False),  # Case with missing license key
+        # Case with missing key in license
+        ({"license": {}}, "my_license", False),
+        # Case with None as key
+        ({"license": {"key": None}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test the has_license method."""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
